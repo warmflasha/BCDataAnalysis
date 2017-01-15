cf8973791bcad666b52c2107d53dbee407481e22
aryeh <warmflasha@gmail.com>
2017-01-15 10:50:12 -0600

    allow to filter only non-baseline batteries

--- dataFilter/filterCompleteBaselineBatteries.m
+++ dataFilter/filterCompleteBaselineBatteries.m
@@ -1,8 +1,18 @@
-function [batteries,inds] = filterCompleteBaselineBatteries(batteries)
+function [batteries,inds] = filterCompleteBaselineBatteries(batteries,baseval)
 %Takes a set of batteries and returns those that are complete and are a
 %baseline test. Also returns inds of those batteries in the original array
+%baseval indicates whether baseline should be true or false (default true)
 
-baseline = [batteries.baseline];
+if ~exist('baseval','var')
+    baseval = true;
+end
+
+if baseval
+    baseline = [batteries.baseline];
+else
+    baseline = ~[batteries.baseline];
+end
+
 nbat = length(batteries);
 iscomplete = false(1,nbat);
 for ii = 1:nbat
